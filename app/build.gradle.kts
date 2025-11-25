c7505773cfabd8bc434c405d953b072aef57ed19
Syahrul Huda <syahrulhuda2910@gmail.com>
2025-11-25 11:46:17 +0700

    repair7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25"
 }
 
 android {
